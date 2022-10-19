7b9665dd92c5ed3ada36f91806b3f72f23a85ff8
Sara-Silvestre <86088494+Sara-Silvestre@users.noreply.github.com>
2022-10-19 14:43:12 -0300

    add photo

--- src/data/team.js
+++ src/data/team.js
@@ -337,7 +337,14 @@ export default [
           'Programadora iOS ex aluna do Developer Academy, estudante do curso Ciência da Computação da UFPE.',
         gitHub: 'https://github.com/marina-barros',
         linkedIn: 'https://www.linkedin.com/in/marina-barros-80b582173/',
-      }
+      },
+      {
+        photo:'https://avatars.githubusercontent.com/u/86088494?s=400&u=34c951dc3fb5b987cf15154067fc6cc0b747a413&v=4',
+        name: '',
+        description: '',
+        gitHub: '',
+        linkedIn: '',
+      },
     ],
   },
 ];
